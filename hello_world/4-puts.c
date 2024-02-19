b7dfdaf209376b8426f486e1ed9d93de92cf933c
khiba-k <khibakoenane@gmail.com>
2024-02-19 07:39:24 -0800

    printf

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *main - is where the function starts
  *return :  value ends function
 */
-int main(void)
+int main(void) /*This is a comment*/
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return (0);/*comment for return*/
 }
