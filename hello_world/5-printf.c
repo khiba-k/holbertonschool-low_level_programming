3e03b9c90cf3bdbf2f0ce1cb67d4f144922fc8c9
khiba-k <khibakoenane@gmail.com>
2024-02-19 23:41:20 -0800

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * Main - beginning of function
- * Return - end of function
+ * main - beginning of function
+ * Return: end of function
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,"); 
-/*return - end of function*/
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
