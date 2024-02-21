5535bcfd9d590bd389a7e46037ccdac1c3b583e3
khiba-k <khibakoenane@gmail.com>
2024-02-21 02:47:31 -0800

    alphaBET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - beginning of function
+ *Return: end of function
+ */
+
+int main(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'Z'; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
