ab07c2fb8a6ec619a0f7da586f61114b27dfeb43
khiba-k <khibakoenane@gmail.com>
2024-02-21 02:11:15 -0800

    last digit

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - function begins
+ *Return: end of function
+ */
+int main(void)
+{
+char ch;
+for (ch == 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+return (0);
+}
