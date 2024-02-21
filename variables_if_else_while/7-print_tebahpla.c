95ab0c7e94811ee1e69564b4ad9bed29ef010139
khiba-k <khibakoenane@gmail.com>
2024-02-21 05:14:55 -0800

    reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - begins function
+ *Return: end of function
+ */
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
