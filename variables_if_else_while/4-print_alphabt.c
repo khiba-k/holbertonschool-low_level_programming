7a209537677b032d4b1965addc496df0cc13fe7f
khiba-k <khibakoenane@gmail.com>
2024-02-21 03:41:27 -0800

    exclude e and q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - begins function
+ *Return: end of function
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' || ch != 'e')
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
