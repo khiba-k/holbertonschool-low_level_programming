565e42f82fea9b813bc3f56ce35d55bf860fdbb9
khiba-k <khibalpemame@gmail.com>
2024-02-22 10:16:06 +0200

    print combo

--- variables_if_else_while/100-print_comb3.s
+++ variables_if_else_while/100-print_comb3.s
@@ -13,8 +13,9 @@ main:
 	.cfi_def_cfa_register 6
 	subq	$16, %rsp
 	movl	$0, -4(%rbp)
+	nop
 	cmpl	$99, -4(%rbp)
-	jg	.L2
+	jg	.L9
 	movl	-4(%rbp), %eax
 	addl	$1, %eax
 	movl	%eax, -8(%rbp)
@@ -26,8 +27,9 @@ main:
 	shrq	$32, %rdx
 	sarl	$2, %edx
 	sarl	$31, %eax
-	subl	%eax, %edx
+	movl	%eax, %ecx
 	movl	%edx, %eax
+	subl	%ecx, %eax
 	addl	$48, %eax
 	movl	%eax, %edi
 	call	putchar@PLT
@@ -35,11 +37,11 @@ main:
 	movslq	%ecx, %rax
 	imulq	$1717986919, %rax, %rax
 	shrq	$32, %rax
+	sarl	$2, %eax
+	movl	%ecx, %esi
+	sarl	$31, %esi
+	subl	%esi, %eax
 	movl	%eax, %edx
-	sarl	$2, %edx
-	movl	%ecx, %eax
-	sarl	$31, %eax
-	subl	%eax, %edx
 	movl	%edx, %eax
 	sall	$2, %eax
 	addl	%edx, %eax
@@ -65,7 +67,7 @@ main:
 	jle	.L6
 	movl	$0, %eax
 	jmp	.L7
-.L2:
+.L9:
 	movl	$0, %eax
 .L7:
 	leave
@@ -74,21 +76,21 @@ main:
 	.cfi_endproc
 .LFE0:
 	.size	main, .-main
-	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
 	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
 0:
-	.string	 "GNU"
+	.string	"GNU"
 1:
 	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
+	.long	0xc0000002
+	.long	3f - 2f
 2:
-	.long	 0x3
+	.long	0x3
 3:
 	.align 8
 4:
