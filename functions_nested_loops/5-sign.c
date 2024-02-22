f87b9b1b3b1e03ca735e6abd4d997eae9fa6214c
khiba-k <khibalpemame@gmail.com>
2024-02-22 17:35:52 +0200

    signs

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *print_sign - function begins
+ *@n: parameter name
+ *Return: what should be returned
+ */
+int print_sign(int n)
+{
+if (n > '0')
+{
+_putchar('+');
+_putchar(',');
+_putchar(' ');
+return (1);
+}
+else if (n == '0')
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+return (0);
+}
+else
+{
+_putchar('-');
+_putchar(',');
+_putchar(' ');
+return (-1);
+}
+}
+
