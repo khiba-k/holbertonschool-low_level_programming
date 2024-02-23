af77b9c3dd3aeb840497167171f0b91a26134403
khiba-k <khibakoenane@gmail.com>
2024-02-23 15:35:55 +0000

    print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,36 +6,22 @@
  */
 void print_to_98(int n)
 {
-if (n >= 98)
-{
-for (; n >= 98; n--)
-{
-if (n == 98)
-{
-printf("%d", n);
-}
-else
+while (n >= 98)
 {
 printf("%d", n);
 _putchar(',');
 _putchar(' ');
+n--;
 }
-}
-}
-else
-{
-for (; n <= 98; n++)
-{
-if (n == 98)
-{
-printf("%d", n);
-}
-else
+while (n <= 98)
 {
 printf("%d", n);
 _putchar(',');
 _putchar(' ');
+n++;
 }
-}
+if (n == 98)
+{
+printf("%d", n);
 }
 }
--- functions_nested_loops/11-print_to_98.s
+++ functions_nested_loops/11-print_to_98.s
@@ -17,47 +17,8 @@ print_to_98:
 	.cfi_def_cfa_register 6
 	subq	$16, %rsp
 	movl	%edi, -4(%rbp)
-	cmpl	$97, -4(%rbp)
-	jle	.L8
-	jmp	.L3
-.L6:
-	cmpl	$98, -4(%rbp)
-	jne	.L4
-	movl	-4(%rbp), %eax
-	movl	%eax, %esi
-	leaq	.LC0(%rip), %rax
-	movq	%rax, %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	jmp	.L5
-.L4:
-	movl	-4(%rbp), %eax
-	movl	%eax, %esi
-	leaq	.LC0(%rip), %rax
-	movq	%rax, %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	movl	$44, %edi
-	call	_putchar@PLT
-	movl	$32, %edi
-	call	_putchar@PLT
-.L5:
-	subl	$1, -4(%rbp)
+	jmp	.L2
 .L3:
-	cmpl	$97, -4(%rbp)
-	jg	.L6
-	jmp	.L12
-.L11:
-	cmpl	$98, -4(%rbp)
-	jne	.L9
-	movl	-4(%rbp), %eax
-	movl	%eax, %esi
-	leaq	.LC0(%rip), %rax
-	movq	%rax, %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	jmp	.L10
-.L9:
 	movl	-4(%rbp), %eax
 	movl	%eax, %esi
 	leaq	.LC0(%rip), %rax
@@ -68,12 +29,35 @@ print_to_98:
 	call	_putchar@PLT
 	movl	$32, %edi
 	call	_putchar@PLT
-.L10:
+	subl	$1, -4(%rbp)
+.L2:
+	cmpl	$97, -4(%rbp)
+	jg	.L3
+	jmp	.L4
+.L5:
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$44, %edi
+	call	_putchar@PLT
+	movl	$32, %edi
+	call	_putchar@PLT
 	addl	$1, -4(%rbp)
-.L8:
+.L4:
 	cmpl	$98, -4(%rbp)
-	jle	.L11
-.L12:
+	jle	.L5
+	cmpl	$98, -4(%rbp)
+	jne	.L7
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+.L7:
 	nop
 	leave
 	.cfi_def_cfa 7, 8
