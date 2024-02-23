cd0b25d27db9405707bdd43d62da4b60ea352b1b
khiba-k <khibakoenane@gmail.com>
2024-02-23 08:17:29 +0000

    absolute num

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *int _abs(int) - function begins
+ *@n: parameter name
+ *Return: to be returned
+ */
+int _abs(int n)
+{
+int num = -n;
+if (n < 0)
+{
+return(num);
+}
+else
+{
+return (n);
+}
+}
