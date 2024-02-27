f60525b4a9fa656224b14aa3c07729c2f806b34b
khiba-k <khibakoenane@gmail.com>
2024-02-27 10:07:05 +0000

    fizz buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ *main - function name
+ *Return: end of function
+ */
+int main(void)
+{
+int num;
+for (num = 1; num < 101; num++)
+{
+if (num % 3 == 0)
+{
+putchar('F');
+putchar('I');
+putchar('Z');
+putchar('Z');
+putchar(' ');
+}
+else if (num % 5 == 0)
+{
+putchar('B');
+putchar('U');
+putchar('Z');
+putchar('Z');
+putchar(' ');
+}
+else
+{
+if (num > 9)
+{
+putchar(num / 10 + '0');
+}
+putchar((num % 10) + '0');
+putchar(' ');
+}
+}
+return (0);
+}
