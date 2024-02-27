7cb62bbaa9d0f54383e359f12b0698ea75bb337d
khiba-k <khibakoenane@gmail.com>
2024-02-27 13:15:46 +0000

    fizz buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 int num;
-for (num = 1; num < 101; num++)
+for (num = 1; num <= 100; num++)
 {
 if (num % 3 == 0 && num % 5 == 0)
 {
@@ -18,13 +18,9 @@ else if (num % 3 == 0)
 printf("Fizz ");
 }
 else if (num % 5 == 0)
-{
 printf("Buzz ");
-}
 else
-{
 printf("%d ", num);
-}
 }
 printf("\n");
 return (0);
