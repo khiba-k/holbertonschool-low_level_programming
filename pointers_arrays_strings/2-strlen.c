28a03b833dd555bad3099703cbfe40cfd196aec7
khiba-k <khibakoenane@gmail.com>
2024-02-28 13:12:28 +0000

    string length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strlen - function returns length of string
+ *Return: returns length of code
+ *@s: variable of string
+ */
+int _strlen(char *s)
+{
+int i;
+int length;
+for (i = 0; s[i] != '\0'; i++)
+{
+length++;
+}
+return (length);
+}
+
