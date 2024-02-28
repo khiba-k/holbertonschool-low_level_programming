c74dfe11ad3a503c45c38f45ed94eacc3f881498
khiba-k <khibakoenane@gmail.com>
2024-02-28 12:29:07 +0000

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *swap_int - function to swap values
+ *@a: value of first integer
+ *@b: value of second integer
+ */
+void swap_int(int *a, int *b)
+{
+int one = *a;  
+ int two = *b;
+ *b = one;
+ *a = two;
+}
+
