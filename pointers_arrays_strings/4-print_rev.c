409e820dbab9d894432b722b8dd76242aef87fea
khiba-k <khibakoenane@gmail.com>
2024-02-29 10:20:16 +0000

    print reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,15 @@
  */
 void print_rev(char *s)
 {
-int i;
+int i = 0;
 int j;
 int k;
-for (i = 0; s[i] != '\0'; i++)
+while (s[i] != '\0')
+i++;
 k = i;
 for (j = k - 1; j >= 0; j--)
 {
 _putchar(s[j]);
 }
+_putchar('\n');
 }
