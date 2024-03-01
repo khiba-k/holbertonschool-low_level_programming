87d65722de6609c46c62145c61918f7c8452c327
khiba-k <khibakoenane@gmail.com>
2024-03-01 08:47:46 +0000

    puts half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,26 +5,17 @@
  */
 void puts_half(char *str)
 {
-int i = 0;
-int len = 0;
-int m = 0;
-while (str[i] != '\0')
-{
+int length, i;
+length = 0;
+while (str[length] != '\0')
+length++;
+i = length / 2;
+if (length % 2 == 1)
 i++;
-}
-if (len % 2 == 0)
+while (i < length)
 {
-for (len = i / 2; str[len] != '\0'; len++)
-{
-_putchar(str[len]);
-}
-}
-else if (len % 2)
-{
-for (m = (len - 1) / 2; m <= len - 1; m++)
-{
-_putchar(str[m + 1]);
-}
+_putchar(str[i]);
+i++;
 }
 _putchar('\n');
 }
