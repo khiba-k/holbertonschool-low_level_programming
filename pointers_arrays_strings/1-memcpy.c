d6d804bc648996a5bc25df4f46b617507fa7eaf4
khiba-k <khibakoenane@gmail.com>
2024-03-04 10:07:14 +0000

    memory copy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * 
- *  
+ *_memcpy - funcion copies elements of one array to another array
+ *@dest: destination parameter
+ *@src: source parameter
+ *@n: number of elements to copy
+ *Return: returns dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  unsigned j, k;
-  for (j = 0; j < n; j++)
-    {
-      for (k = 0; k < n; k++)
-	{
-	  dest[k] = src[j];
-	}
-    }
-  return (dest);
+unsigned int j;
+for (j = 0; j < n; j++)
+{
+dest[j] = src[j];
+}
+return (dest);
 }
