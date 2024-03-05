5f02cb12e4c338616c6075f137483958e9f614b6
khiba-k <khibakoenane@gmail.com>
2024-03-05 09:59:57 +0000

    chess board

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,14 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * 
- *  
+ *print_chessboard - function prints chess board
+ *@a: array
  */
 void print_chessboard(char (*a)[8])
 {
-  int i;
-  for (i = 0; (*a)[i] != '\0'; i++)
-    {
-_putchar(*a);
-    }
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+putchar(a[i][j]);
+}
+putchar('\n');
+}
 }
