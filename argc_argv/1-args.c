b27e42e6b3b9b3fcbb8a0393e0f97d4d97dca778
khiba-k <khibakoenane@gmail.com>
2024-03-07 15:01:13 +0000

    argument count

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function prints number of arguments
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: end of function
+*/
+int main(int argc, char *argv[])
+{
+printf("%d\n", i);
+return (0);
+}
