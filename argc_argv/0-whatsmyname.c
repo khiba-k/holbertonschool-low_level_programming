ef7f1b9a337a62f1a3dd30f4075364dcfe698483
khiba-k <khibakoenane@gmail.com>
2024-03-07 14:36:03 +0000

    my new name is

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 /**
- *
+ *main - function prints name of program
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: end of function
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  int i;
-  for (i = 0; i < argc; i++)
-    {
-  printf("%s\n\n", argv[i]);
-    }
-  return (0);
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
