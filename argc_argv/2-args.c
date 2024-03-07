0d7341ea5a4aa0611d74f7497579f4a4e9f8bc11
khiba-k <khibakoenane@gmail.com>
2024-03-07 16:23:56 +0000

    prints all arguments

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /**
- * 
- * 
+ *main - function prints arguments
+ *@argc: argument counter
+ *@argv: argument vector
+ *Return: end of function
 */
 int main(int argc, char *argv[])
 {
-  int i;
-  for (i = 0; i < argc; i++)
-    {
-      printf("%s", argv[i]);
-    }
-  return (0);
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
