7380b62ab4ba7fb5476735a58486186a6e01d310
khiba-k <khibakoenane@gmail.com>
2024-03-09 08:58:12 +0000

    product of arguments

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -5,7 +5,7 @@
  *@argc: argument count
  *@argv: argument vector
  *Return: what to return/ end of function
-  */
+ */
 int main(int argc, char *argv[])
 {
 int i, j;
@@ -14,7 +14,7 @@ if (argc == 3)
 {
 i = atoi(argv[1]);
 j = atoi(argv[2]);
-printf("%d", i * j);
+printf("%d\n", i *j);
 return (0);
 }
 else
