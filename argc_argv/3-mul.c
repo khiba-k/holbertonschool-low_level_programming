adf7be075fbcdd8b77193d2adeb5f78d8377e3e9
khiba-k <khibakoenane@gmail.com>
2024-03-09 08:22:48 +0000

    product of arguments

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * 
- * 
-*/
+ *main - function prints product of two numbers
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: what to return/ end of function
+  */
 int main(int argc, char *argv[])
 {
-  int i;
-  int j;
-  for (i = 0; i < argc; i++)
-    {
-      if (i > 0)
-	{
-	  j = argv[i] - '0';
-	printf("%d", j * j);
-	}
-    }
-  return (Error);
+int i, j;
+i = j = 0;
+if (argc == 3)
+{
+i = atoi(argv[1]);
+j = atoi(argv[2]);
+printf("%d", i * j);
+return (0);
+}
+else
+printf("Error\n");
+return (1);
 }
