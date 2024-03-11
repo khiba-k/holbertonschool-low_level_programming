1d5ab06be70b8947a0134580f533f0256bc78297
khiba-k <khibakoenane@gmail.com>
2024-03-11 14:02:01 +0000

    string duplicate

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,10 +12,10 @@ char *_strdup(char *str)
 	char *s;
 	int i, j;
 
+	if (str == NULL)
+			return (NULL);
 	for (j = 0; str[j] != '\0'; j++)
 		;
-		if (str == NULL)
-			return (NULL);
 		s = malloc(j * sizeof(*str) + 1);
 		if (s == NULL)
 			return (NULL);
