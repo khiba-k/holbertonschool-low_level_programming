95c74ab93291285434c1740acd70c6333cbbb84d
khiba-k <khibakoenane@gmail.com>
2024-03-12 10:19:50 +0000

    cat strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -4,26 +4,39 @@
 #include <string.h>
 
 /**
- * 
- * 
+ *str_concat - function concatenates two strings
+ *@s1: parameter for first string
+ *@s2: parameter for second string
+ *Return: value to be returned/ function ends
 */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, len1 = 0,len2 = 0, sum;
+	int i, j, len1 = 0, len2 = 0, sum, cat_sum = 0;
 	char *c;
 
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
+	if (s2 == NULL)
+	{
+		s2 = " ";
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		len1++;
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		len2++;
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+			len2++;
+		}
 	}
 	sum = len1 + len2;
 	c = malloc(sum * sizeof(c));
-        strcpy(c, s1);
-        strcat(c, s2);
+	for (sum = 0; s1[sum]; sum++)
+		c[cat_sum++] = s1[sum];
+	for (sum = 0; s2[sum]; sum++)
+		c[cat_sum++] = s2[sum];
+
 	return (c);
 	if (c == NULL)
 	{
