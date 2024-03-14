33409cd1a615e2957fc948b0d93572a3faa11d70
khiba-k <khibakoenane@gmail.com>
2024-03-14 13:25:20 +0000

    structures

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,16 @@
-struct dog {
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *struct dog - structure
+ *@name: name of dog
+ *@age: age of owner
+ *@owner: name of dog ownet
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
