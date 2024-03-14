16256a25f7d0b38e3017ae51a673dd6a2dbb4b4f
khiba-k <khibakoenane@gmail.com>
2024-03-14 14:10:28 +0000

    initialize variable

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -13,4 +15,6 @@ struct dog
 	float age;
 	char *owner;
 };
+#endif /* DOG_H */
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
