635942bb35b34e8eefff606aa2dff33db3ded190
khiba-k <khibakoenane@gmail.com>
2024-03-14 09:43:41 +0000

    string concatenate

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (j = 0; j < n; j++)
 		;
 	sum = len + j;
-	c = malloc((sum + 1) * sizeof(c));
+	c = malloc((sum + 1) * sizeof(char));
 	for (i = 0; s1[i] != '\0'; i++)
 		c[cat++] = s1[i];
 	for (j = 0; j < n && s2[j] != '\0'; j++)
