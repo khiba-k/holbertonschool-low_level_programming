46046915fd69c429003b947bc2bafdf60fb9d01c
khba-k <khibakoenane@gmail.com>
2024-03-15 10:19:26 +0000

    structures

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * 
+ * 
+*/
+void print_dog(struct dog *d)
+{
+printf("%type", *d);
+
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,12 +9,13 @@
  *@owner: name of dog ownet
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} type;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif /* DOG_H */
