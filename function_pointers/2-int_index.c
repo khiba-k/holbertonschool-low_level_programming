a6aea35be57bf40d098b441d60e0153a91852509
khiba-k <khibakoenane@gmail.com>
2024-03-18 12:06:06 +0000

    integer index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,13 +12,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
 	for (i = 0; i < size; i++)
 		if ((*cmp)(array[i]))
 		{
 			return (i);
 		}
-	if (size <= 0 || (!array) || (!cmp))
-		return (-1);
-
 	return (-1);
 }
