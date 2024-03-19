c4e30c81fbca1926a1c570b226e1c2d5dff4c57b
khiba-k <khibakoenane@gmail.com>
2024-03-19 10:49:00 +0000

    program performs simple operations

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 #include "3-calc.h"
 /**
- *
- *
+ *main - function pefoms simple operations
+ *@argc: argument count
+ *@argv: value of arguments
+ *arg1: value of first argument
+ *arg3: value of third argument
+ *Return: value to return
  */
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 
 	int arg1, arg3;
 
 	if (argc != 4)
 	{
-		printf("Error\n"),exit(98);
+		printf("Error\n"), exit(98);
 	}
 	arg1 = atoi(argv[1]);
 	arg3 = atoi(argv[3]);
-	if (argc > 4 || (*argv[2] == '/' && arg3 == 0) || (*argv[2] == '%' && arg3 == 0))	
+	if (argv[3] == 0 && (*argv[2] == '/' || *argv[2] == '%'))
 	{
-		printf("Error\n"),exit(100);
+		printf("Error\n"), exit(100);
 	}
-	if(get_op_func(argv[2]) == NULL)
+	if (get_op_func(argv[2]) == NULL)
 	{
-		printf("Error\n"),exit (99);
+		printf("Error\n"), exit(99);
 	}
 	printf("%d\n", (get_op_func(argv[2])(arg1, arg3)));
 	return (0);
