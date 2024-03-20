82edeea66c3bab087de2b8487cbf8b77327760f0
khiba-k <khibakoenane@gmail.com>
2024-03-20 10:56:04 +0000

    print numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		j = va_arg(ap, int);
 		printf("%d", j);
-		if (*separator != '\0' && i != n - 1)
+		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
