65d1106af94cebc29444e0fa8fc1a9560c3869ca
khiba-k <khibalpemame@gmail.com>
2024-04-03 13:52:25 +0200

    get nth element

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *get_dnodeint_at_index - function returns the nth element
+ *@head: structure pointer
+ *@index: the index of the node
+ *Return: value to be returned
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	 unsigned int j = 0;
+
+	while (j < index)
+	{
+		head = head->next;
+		j++;
+	}
+	return (head);
+}
