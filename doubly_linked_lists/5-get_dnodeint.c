86475569d4d0f5738015fad21e614fcee08e50f5
khiba-k <khibalpemame@gmail.com>
2024-04-05 11:14:49 +0200

    fixes

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -13,6 +13,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
+	while (head->prev != NULL)
+		head = head->prev;
 	if (index == j)
 		return (head);
 	while (j < index)
