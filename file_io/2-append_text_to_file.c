2f333e33e276c049b91dd717147a16f7bf1696c3
khiba-k <khibalpemame@gmail.com>
2024-04-06 13:43:59 +0200

    append file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,19 +14,21 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-        int fd, writef, i = 0;
+	int fd, writef, i = 0;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        fd = open(filename, O_RDWR | O_APPEND, 0666);
-        if (fd == -1)
-                return (-1);
-        while (text_content[i] != '\0')
-                i++;
-        writef = write(fd, text_content, i);
-        if (writef == -1)
-                return (-1);
+	if (text_content == NULL)
+		return (1);
+	fd = open(filename, O_RDWR | O_APPEND, 0666);
+	if (fd == -1)
+		return (-1);
+	while (text_content[i] != '\0')
+		i++;
+	writef = write(fd, text_content, i);
+	if (writef == -1)
+	return (-1);
 
-        return (1);
+	return (1);
 }
